2c0303128b4aab8451c00c40fb6e389e276d81f7
lisette-git <95486863+lisette-git@users.noreply.github.com>
2022-05-26 10:18:35 +0200

    primecheck implemented

--- src/app/components/primenumber/primenumber.component.ts
+++ src/app/components/primenumber/primenumber.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { TheGreatService } from '../../services/thegreat.service';
 
 @Component({
   selector: 'app-primenumber',
   templateUrl: './primenumber.component.html',
-  styleUrls: ['./primenumber.component.css']
+  styleUrls: ['./primenumber.component.css'],
+  providers: [TheGreatService]
 })
 export class PrimenumberComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: TheGreatService) { }
 
   ngOnInit() {
   }
+
+  numb = '';
+  primeresult = '';
+
+
+    clickPrimeCheck() {
+
+    this.numb = (<HTMLInputElement>document.getElementById("numin")).value;
+
+      const soso = this.service.primecheck(this.numb)
+      .subscribe((response: any) =>  this.primeresult = response.toString());
+      
+
+      
+    }
 
 }
--- src/app/services/thegreat.service.ts
+++ src/app/services/thegreat.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,11 +7,24 @@ import { Observable } from 'rxjs';
 })
 export class TheGreatService {
 constructor(private http: HttpClient){
+  
 }
 
 reversalString(text): Observable<string> {
 return this.http.post("http://localhost:8081/game/reversal/",text,{responseType: 'text'});
 };
+
+
+
+primecheck(nummie): Observable<Object> {
+  // let headers = new HttpHeaders().append('in', nummie);
+  // headers.append('responseType','text');
+  // const headers= new HttpHeaders()
+  // .set('in',nummie.toString());
+  return this.http.post("http://localhost:8081/game/prime-number-check/",'',{headers: new HttpHeaders({ 'in2': nummie , 'responseType' : 'text' })});
+  };
+
+
 
 
 
